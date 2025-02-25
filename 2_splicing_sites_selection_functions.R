3dbe1afd8d442a38a16a47c101afae1b673f669b
Nadzeya <boyevanadzeya@gmail.com>
2025-02-25 18:01:23 +0300

    Update 2_splicing_sites_selection_functions.R

--- 2_splicing_sites_selection_functions.R
+++ 2_splicing_sites_selection_functions.R
@@ -1,109 +1,83 @@
-filter_eej_v <- function(v, 
-                         use_raw=FALSE, 
-                         raw_threshold=10, 
-                         cpm_threshold=-1, 
-                         calculate_cpm_threshold=FALSE) {
+get_sum_vector <- function(df) {
   
-  # This function takes vector, calculates cpm and threshold for filtration
-  
-  if (use_raw) {
-    return(as.integer(v >= raw_threshold))
-  } 
-  else {
-    dens <- density(cpm(v, log=TRUE, prior.count=0.5))
-    
-    if (calculate_cpm_threshold) { # reconsider
-      der <- c(diff(dens$y), tail(diff(dens$y), 1)) > 0
-      cpm_threshold <- tail(dens$x[c(FALSE, diff(der) != 0)], 2)[1]
-    }
-    
-    return(as.integer(cpm(v, log=TRUE, prior.count=0.5) >= cpm_threshold))
-  }
+  sum_vector <- rowSums(df[, !(colnames(df) %in% 
+                                 c("eej_id", "gene_id", "seqnames", 
+                                   "start", "end", "width", "strand", 
+                                   "intron_length", "sum_samples_cpm", 
+                                   "sum_samples_raw", "sum_cpm", 
+                                   "sum_raw"))], 
+                        na.rm = TRUE)
+  return(sum_vector)
 }
-
-
 
 filter_eej_cols <- function(df,
                             use_raw=FALSE, 
                             raw_threshold=10, 
-                            cpm_threshold=-1, 
-                            calculate_cpm_threshold=FALSE) {
+                            cpm_threshold=-1) {
   
   # This function applies filter_eej_v() to all columns in the dataframe given
   
-  cols <- list()
-  for (col in colnames(df)) {
-    col_filter <- filter_eej_v(df[col],
-                               use_raw, 
-                               raw_threshold, 
-                               cpm_threshold, 
-                               calculate_cpm_threshold)
-    cols[[col]] <- col_filter
+  if (use_raw) {
+    sum_vector <- get_sum_vector(df)
+    df_filtered <- as.data.frame(lapply(df, 
+                                        function(x) { return(as.integer(x >= raw_threshold)) }))
+  } else {
+    df_filtered <- as.data.frame(lapply(df, 
+                                        cpm, log=TRUE, prior.count=0.5))
+    sum_vector <- get_sum_vector(df_filtered)
+    df_filtered <- as.data.frame(lapply(df, 
+                                        function(x) { return(as.integer(x >= raw_threshold)) }))
   }
-  df <- data.frame(cols)
-  return(df)
+  
+  return(list(df_filtered, sum_vector))
 }
 
 
 
 filter_eej_df <- function(full_df,
-                          use_raw=FALSE, 
+                          use_raw=FALSE,
+                          use_cpm=FALSE,
                           raw_threshold=10, 
                           cpm_threshold=-1, 
-                          calculate_cpm_threshold=FALSE) {
+                          calculate_cpm_threshold=FALSE,
+                          n_samples_threshold=1) {
   
   # This function applies filter_eej_cols() to initial file
   
   meta_df <- full_df[, colnames(full_df) %in% 
                        c("eej_id", "gene_id", "seqnames", 
-                       "start", "end", "width", "strand")]
+                       "start", "end", "width", "strand",
+                       "intron_length", "sum_samples_cpm", 
+                       "sum_samples_raw", "sum_cpm", 
+                       "sum_raw")]
   matrix_df <- full_df[, !(colnames(full_df) %in% 
-                         c("eej_id", "gene_id", "seqnames", 
-                        "start", "end", "width", "strand", "intron_length"))]
-  df_filter <- filter_eej_cols(matrix_df,
-                               use_raw,
-                               raw_threshold,
-                               cpm_threshold,
-                               calculate_cpm_threshold)
-  df <- cbind(meta_df, df_filter)
-  return(df)
-}
-
-
-
-add_sum_column <- function(df) {
+                             c("eej_id", "gene_id", "seqnames", 
+                               "start", "end", "width", "strand", 
+                               "intron_length", "sum_samples_cpm", 
+                               "sum_samples_raw", "sum_cpm", 
+                               "sum_raw"))]
   
-  # This function adds to the dataframe a column with number of samples where EEJ passed the threshold
-  
-  df$sum_samples <- rowSums(df[, !(colnames(df) %in% 
-                                    c("eej_id", "gene_id", "seqnames", 
-                                    "start", "end", "width", "strand", 
-                                    "intron_length", "sum_samples"))], 
-                            na.rm = TRUE)
-  return(df)
-}
+  if (use_raw) {
 
-
-
-get_eej <- function(df,
-                    use_raw=FALSE, 
-                    raw_threshold=10, 
-                    cpm_threshold=-1, 
-                    calculate_cpm_threshold=FALSE) {
+    list_filter <- filter_eej_cols(matrix_df,
+                                 use_raw=TRUE,
+                                 raw_threshold)
+    meta_df$sum_raw <- list_filter[[2]]
+    meta_df$sum_samples_raw <- get_sum_vector(list_filter[[1]])
+    filter_vector <- meta_df$sum_samples_raw >= n_samples_threshold
+    meta_df <- meta_df[filter_vector,]
+    matrix_df <- matrix_df[filter_vector,]
+  }
   
-  # This function returns subset of the dataframe with EEJ which passed the threshold at least in one sample
+  if (use_cpm) {
+    list_filter <- filter_eej_cols(matrix_df,
+                                 use_raw=FALSE,
+                                 cpm_threshold)
+    meta_df$sum_cpm <- list_filter[[2]]
+    meta_df$sum_samples_cpm <- get_sum_vector(list_filter[[1]])
+    filter_vector <- meta_df$sum_samples_cpm >= n_samples_threshold
+    meta_df <- meta_df[filter_vector,]
+  }
   
-  df_filter <- filter_eej_df(df,
-                             use_raw, 
-                             raw_threshold, 
-                             cpm_threshold, 
-                             calculate_cpm_threshold)
-  df_filter <- add_sum_column(df_filter)
-  df_filtered <- df_filter[df_filter$sum_samples > 0,]
-  
-  print(paste0('All EEJs: ', nrow(df_filter)))
-  print(paste0('Filtered EEJs: ', nrow(df_filtered)))
-  print(paste0('Filtered out: ', nrow(df_filter) - nrow(df_filtered)))
-  
-  return(df_filtered)
+  return(meta_df)
 }
